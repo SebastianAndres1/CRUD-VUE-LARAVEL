c5f2f7fb0936780d9f83a4a5cc1d4bf050b7c762
SebastianAndres1 <18seba16@gmail.com>
2021-06-16 19:12:40 -0400

    agregar vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,11 @@
 require('./bootstrap');
+
+import Vue from 'vue'
+import Home from './components/Home'
+
+const vue = new Vue({
+    el:'#app',
+    components:{
+        Home
+    }
+})
---
+++ resources/js/components/Home.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container-fluid page">
+        <div class="row">
+                <h1>CRUD WITH <span class="text-success">VUEJS</span></h1>
+        </div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+
+
+</style>
+
